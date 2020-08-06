78f1173708f51ec7f08a963f7a942e9ea9e4ab7b
wizweishijun <wsj@wiz.cn>
2020-08-06 18:38:52 +0800

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const assert = require('assert');
 const users = require('./user/users');
-const { registerListener } = require('./user/users');
 
 assert(global.wizWrapper, 'wizWrapper must be initialized before using wiznote sdk');
 
